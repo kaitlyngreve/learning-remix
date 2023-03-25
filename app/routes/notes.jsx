d91d61d4eb03fb1a06234b993c7519e46f57b549
Kaitlyn Greve <kgreve14@gmail.com>
2023-03-25 10:51:38 -0700

    made server side form work

--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,4 +1,6 @@
+import { redirect } from "react-router";
 import NewNote, { links as newNoteLinks } from "~/components/NewNote";
+import { getStoredNotes, storeNotes } from "~/data/notes";
 
 export default function NotesPage() {
   return (
@@ -6,6 +8,25 @@ export default function NotesPage() {
       <NewNote />
     </main>
   );
+}
+
+// the 'request' paramter is just data.request destructured
+export async function action({ request }) {
+  const formData = await request.formData();
+  const noteData = Object.fromEntries(formData);
+  // OR - this does the same thing
+  // const noteData = {
+  //   title: formData.get("title"),
+  //   content: formData.get("content"),
+  // };
+
+  const existingNotes = await getStoredNotes();
+  noteData.id = new Date().toISOString();
+
+  const updatedNotes = existingNotes.concat(noteData);
+  await storeNotes(updatedNotes);
+
+  return redirect("/notes");
 }
 
 export function links() {
